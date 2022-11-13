f8c72ced4d45ed182599f4adf22a62a93f121e7c
Denys-Fadieiev <dencheek0911@gmail.com>
2022-11-13 09:20:52 +0200

    add Goods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Goods from './components/Goods';
 
-function App() {
+// const iphoneX = {
+//   name: 'Iphone X',
+//   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+//   price: '299 $',
+//   amount: 2,
+// };
+
+// const iphoneXI = {
+//   name: 'Iphone XI',
+//   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+//   price: '349 $',
+//   amount: 0,
+// };
+
+// const iphoneXII = {
+//   name: 'Iphone XII',
+//   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+//   price: '599 $',
+//   amount: 4,
+// };
+
+// ИЛИ
+
+  const Product = function (name, description, price, amount) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.amount = amount;
+  };
+  
+  const iphoneX = new Product('Iphone X', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt', '299 $', 5); 
+  const iphoneXI = new Product('Iphone XI', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt', '349 $', 0); 
+  const iphoneXII = new Product('Iphone XII', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt', '599 $', 5); 
+
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Goods 
+      product= {iphoneX}/>
+      <Goods 
+      product= {iphoneXI}/>
+      <Goods 
+      product= {iphoneXII}/>
+    </>
+    );
+  }
 
 export default App;
